f2cf13fd33b772a0d8de773d960821d79393a493
diegoalva91 <diegoalva91@outlook.com>
2026-09-22 00:30:03 +0200

    fix : backend api location

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-BACKEND_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "backend")
+BACKEND_DIR = os.path.dirname(os.path.dirname(__file__))
 if BACKEND_DIR not in sys.path:
     sys.path.insert(0, BACKEND_DIR)
 
